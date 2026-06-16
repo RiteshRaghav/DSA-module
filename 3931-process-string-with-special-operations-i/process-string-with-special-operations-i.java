0d9ad7ec5fb249e4d68cc5e83b46aebd4aa5ebd6
Ritesh_Raghav <ritikraghav424@gmail.com>
2026-06-16 14:14:22 +0530

    Time: 3 ms (100.00%) | Memory: 55 MB (80.88%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='*'){
+                if(sb.length()>0){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }
+            else if(s.charAt(i)=='#'){
+                StringBuilder dup=sb;
+                sb.append(dup);
+            }
+            else if(s.charAt(i)=='%'){
+                sb.reverse();
+            }
+            else{
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
